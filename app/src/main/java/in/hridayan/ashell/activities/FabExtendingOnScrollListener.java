9f5ecf513e2b2c850210efcc8c22c7f1fba231fc
Hridayan <spirriyflix@gmail.com>
2024-01-29 09:10:36 +0530

    ignore

--- app/src/main/java/in/hridayan/ashell/activities/FabExtendingOnScrollListener.java
+++ app/src/main/java/in/hridayan/ashell/activities/FabExtendingOnScrollListener.java
@@ -6,27 +6,30 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 
 public class FabExtendingOnScrollListener extends RecyclerView.OnScrollListener {
 
-  private final ExtendedFloatingActionButton fab;
+    private final ExtendedFloatingActionButton fab;
+    private static final int FAST_SCROLL_THRESHOLD = 25; // Adjust as needed
 
-  public FabExtendingOnScrollListener(ExtendedFloatingActionButton fab) {
-    this.fab = fab;
-  }
-
-  @Override
-  public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
-    super.onScrollStateChanged(recyclerView, newState);
-    if (recyclerView.computeVerticalScrollOffset() == 0) { // Top of the list
-      fab.extend();
+    public FabExtendingOnScrollListener(ExtendedFloatingActionButton fab) {
+        this.fab = fab;
     }
-  }
 
-  @Override
-  public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
-    super.onScrolled(recyclerView, dx, dy);
-    if (dy > 0 && fab.isExtended()) { // Scroll down
-      fab.shrink();
-    } else if (dy < 0 && !fab.isExtended()) { // Scroll up
-      fab.extend();
+    @Override
+    public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+        if (newState == RecyclerView.SCROLL_STATE_IDLE) {
+            if (recyclerView.computeVerticalScrollOffset() == 0) { // Top of the list
+                fab.extend();
+            }
+        }
     }
-  }
+
+    @Override
+    public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        if (dy > 0 && fab.isExtended()) { // Scroll down
+            fab.shrink();
+        } else if (dy < 0 && !fab.isExtended() && Math.abs(dy) >= FAST_SCROLL_THRESHOLD) { // Fast scroll up
+            fab.extend();
+        }
+    }
 }
