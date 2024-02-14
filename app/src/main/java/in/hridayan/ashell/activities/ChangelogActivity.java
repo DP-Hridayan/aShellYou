d85a4c530a4e237163a9b31eb38d70dfbeabb475
DP-Hridayan <hridayanofficial@gmail.com>
2024-02-14 22:09:27 +0530

    bump to v2.0.1

--- app/src/main/java/in/hridayan/ashell/activities/ChangelogActivity.java
+++ app/src/main/java/in/hridayan/ashell/activities/ChangelogActivity.java
@@ -36,33 +36,45 @@ public class ChangelogActivity extends AppCompatActivity {
 
     List<ChangelogItem> changelogItems = new ArrayList<>();
 
+	changelogItems.add(
+        new ChangelogItem(
+            "Version 2.0.1",
+            "\n\n• Removed unnecessary code and resources.\n\n• Minor UI changes."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 2.0.0",
             "\n\n• Changed app name to aShell You.\n\n• Added predictive back animations for supporting devices.\n\n• Added confirmation dialog popup before clearing screen.\n\n• Fixed StatusBar elements not properly visible in Light mode.\n\n• App is much more smooth now.\n ~ Reduced app size and optimised code.\n\n• Added themed icon for launchers that support themed icons.\n\n• Minor UI changes."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 1.3.0",
             "\n\n• Revamped Settings panel.\n\n• Added Double Tap to instant scroll to top and bottom positions in shell output view.\n\n• Minor UI fixes."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 1.2.0",
             "\n\n• Added Settings panel.\n\n• Added option to disable smooth scroll in shell output.\n\n• Fixed major bugs :\n\n    ~ Crash while changing device theme.\n\n    ~ Crash while trying to save large shell output . eg : output of 'pm' command."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 1.1.1", "\n\n• Replaced some deprecated api.\n\n• Minor UI changes."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 1.1.0",
             "\n\n• Added Scroll To Top and Scroll To Bottom buttons.\n\n• Added Click keyboard Enter key to Send command.\n\n• Added some commands.\n\n• Updated dependencies to include latest material you library.\n\n• Minor UI changes."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 1.0.0",
             "\n\n• Added some more command examples.\n\n• Minor UI changes.\n\n• Changed package name to avoid version confusion with original app."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 0.9.1",
             "\n\n• Added changelogs into the app.\n\n• Added highlighted shell output.\n\n• Minor UI changes."));
+		
     changelogItems.add(
         new ChangelogItem(
             "Version 0.9",
