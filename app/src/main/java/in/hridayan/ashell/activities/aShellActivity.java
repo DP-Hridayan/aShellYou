e233a29d56abf80810a43a533e29ded971cff43a
DP-Hridayan <hridayanofficial@gmail.com>
2024-03-08 15:58:55 +0530

    ignore

--- app/src/main/java/in/hridayan/ashell/activities/aShellActivity.java
+++ app/src/main/java/in/hridayan/ashell/activities/aShellActivity.java
@@ -1,12 +1,15 @@
 package in.hridayan.ashell.activities;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.view.View;
-import android.content.DialogInterface;
 import android.view.ViewTreeObserver;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.preference.PreferenceManager;
 import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -18,113 +21,126 @@ import in.hridayan.ashell.utils.ShizukuShell;
 
 public class aShellActivity extends AppCompatActivity {
   private BottomNavigationView mNav;
-  private View mContentView;
-
-  private ShizukuShell mShizukuShell;
-
+  
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_ashell);
 
-    mContentView = findViewById(android.R.id.content);
     mNav = findViewById(R.id.bottom_nav_bar);
+   
+   setupKeyboardVisibilityListener();     
+    setupNavigation();
+    setBadge(R.id.nav_otgShell, "Beta");
+    setBadge(R.id.nav_wireless, "Soon");
+        
 
+  }
+
+  private void setupNavigation() {
     mNav.setVisibility(View.VISIBLE);
-
-    mContentView
-        .getViewTreeObserver()
-        .addOnGlobalLayoutListener(
-            new ViewTreeObserver.OnGlobalLayoutListener() {
-              @Override
-              public void onGlobalLayout() {
-                int heightDiff = mContentView.getRootView().getHeight() - mContentView.getHeight();
-                if (heightDiff > 200) {
-                  mNav.setVisibility(View.GONE);
-                } else {
-                  new Handler(Looper.getMainLooper())
-                      .postDelayed(
-                          new Runnable() {
-                            @Override
-                            public void run() {
-                              mNav.setVisibility(View.VISIBLE);
-                            }
-                          },
-                          100);
-                }
-              }
-            });
-
-    BadgeDrawable badge = mNav.getOrCreateBadge(R.id.nav_otgShell);
-    badge.setVisible(true);
-    badge.setText("Beta");
     mNav.setOnItemSelectedListener(
         item -> {
           switch (item.getItemId()) {
             case R.id.nav_localShell:
-              if ((getSupportFragmentManager().findFragmentById(R.id.fragment_container)
-                  instanceof otgFragment)) {
-                getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.fragment_container, new aShellFragment())
-                    .commit();
-              }
-
+              showaShellFragment();
               return true;
             case R.id.nav_otgShell:
-              if ((getSupportFragmentManager().findFragmentById(R.id.fragment_container)
-                  instanceof aShellFragment)) {
-                // Don't show again logic
-                if (PreferenceManager.getDefaultSharedPreferences(this)
-                    .getBoolean("Don't show beta otg warning", true)) {
-                  MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
-                  builder
-                            .setCancelable(false)
-                      .setTitle("Warning")
-                      .setMessage(getString(R.string.beta_warning))
-                      .setPositiveButton(
-                          "Accept",
-                          (dialogInterface, i) -> {
-                            getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.fragment_container, new otgFragment())
-                                .commit();
-                          })
-                      .setNegativeButton(
-                          "Go back",
-                          (dialogInterface, i) -> {
-                            mNav.setSelectedItemId(R.id.nav_localShell);
-                          })
-                      .setNeutralButton(
-                          "Don't show again",
-                          (dialogInterface, i) -> {
-                            PreferenceManager.getDefaultSharedPreferences(this)
-                                .edit()
-                                .putBoolean("Don't show beta otg warning", false)
-                                .apply();
-
-                            getSupportFragmentManager()
-                                .beginTransaction()
-                                .replace(R.id.fragment_container, new otgFragment())
-                                .commit();
-                          })
-                      .show();
-
-                } else {
-                  getSupportFragmentManager()
-                      .beginTransaction()
-                      .replace(R.id.fragment_container, new otgFragment())
-                      .commit();
-                }
-              }
+              showOtgFragment();
               return true;
             default:
               return false;
           }
         });
+    // Set initial fragment
+    replaceFragment(new aShellFragment());
+  }
+
+  private void replaceFragment(Fragment fragment) {
+
     getSupportFragmentManager()
         .beginTransaction()
-        .replace(R.id.fragment_container, new aShellFragment())
+        .replace(R.id.fragment_container, fragment)
         .commit();
   }
+
+  private void showOtgFragment() {
+    if (!(getSupportFragmentManager().findFragmentById(R.id.fragment_container)
+        instanceof otgFragment)) {
+      // Don't show again logic
+      if (PreferenceManager.getDefaultSharedPreferences(this)
+          .getBoolean("Don't show beta otg warning", true)) {
+        showBetaWarning();
+      } else {
+        replaceFragment(new otgFragment());
+      }
+    }
+  }
+
+  private void showaShellFragment() {
+    if (!(getSupportFragmentManager().findFragmentById(R.id.fragment_container)
+        instanceof aShellFragment)) {
+      replaceFragment(new aShellFragment());
+    }
+  }
+
+  private void showBetaWarning() {
+    MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+    builder
+        .setCancelable(false)
+        .setTitle("Warning")
+        .setMessage(getString(R.string.beta_warning))
+        .setPositiveButton(
+            "Accept",
+            (dialogInterface, i) -> {
+              replaceFragment(new otgFragment());
+            })
+        .setNegativeButton(
+            "Go back",
+            (dialogInterface, i) -> {
+              mNav.setSelectedItemId(R.id.nav_localShell);
+            })
+        .setNeutralButton(
+            "Don't show again",
+            (dialogInterface, i) -> {
+              PreferenceManager.getDefaultSharedPreferences(this)
+                  .edit()
+                  .putBoolean("Don't show beta otg warning", false)
+                  .apply();
+              replaceFragment(new otgFragment());
+            })
+        .show();
+  }
+
+  private void setBadge(int id, String text) {
+    BadgeDrawable badge = mNav.getOrCreateBadge(id);
+    badge.setVisible(true);
+    badge.setText(text);
+    badge.setHorizontalOffset(0);
+  }
+   private void setupKeyboardVisibilityListener() {
+    View contentView = findViewById(android.R.id.content);
+    contentView.getViewTreeObserver().addOnGlobalLayoutListener(new KeyboardVisibilityListener(contentView));
+}
+
+private class KeyboardVisibilityListener implements ViewTreeObserver.OnGlobalLayoutListener {
+    private final View contentView;
+        
+    KeyboardVisibilityListener(View contentView) {
+        this.contentView = contentView;
+    }
+
+    @Override
+    public void onGlobalLayout() {
+        int heightDiff = contentView.getRootView().getHeight() - contentView.getHeight();
+        if (heightDiff > 200) {
+            mNav.setVisibility(View.GONE);
+        } else {
+            new Handler(Looper.getMainLooper()).postDelayed(() -> {
+                mNav.setVisibility(View.VISIBLE);
+            }, 100);
+        }
+    }
+}
+    
 }
