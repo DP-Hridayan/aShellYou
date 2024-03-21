63f2f56769663df16d61097dccfe1e736fc1c921
Krishna <125943937+Krishna-G-OP@users.noreply.github.com>
2024-03-21 21:01:32 +0530

    Delete app directory

--- app/src/main/java/in/hridayan/ashell/Const.java
+++
@@ -1,9 +0,0 @@
-package in.hridayan.ashell.utils;
-
-/**
- * Created by xudong on 2/7/14.
- */
-public class Const {
-    public static final String TAG = "flashbot";
-    public static final double PUSH_PERCENT = 0.5;
-}
