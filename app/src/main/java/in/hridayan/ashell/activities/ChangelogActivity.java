0e125dccf01bd11c904496dd76738e23e40371ab
DP-Hridayan <hridayanofficial@gmail.com>
2024-03-24 01:27:59 +0530

    v3.7.0

--- app/src/main/java/in/hridayan/ashell/activities/ChangelogActivity.java
+++ app/src/main/java/in/hridayan/ashell/activities/ChangelogActivity.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class ChangelogActivity extends AppCompatActivity {
 
-  private final String[] versionNumbers = {
+  private final String[] versionNumbers = {"3.7.0",
     "3.6.0", "3.5.1", "3.5.0", "3.4.0", "3.3.0", "3.2.0", "3.1.0", "3.0.0",
     "2.0.2", "2.0.1", "2.0.0", "1.3.0", "1.2.0", "1.1.1", "1.1.0", "1.0.0",
     "0.9.1", "0.9.0"
