83d489099170663759e0020ff2fc5438839011d7
dp-hridayan <hridayanofficial@gmail.com>
2025-06-07 14:27:36 +0530

    WIP

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "in.hridayan.ashell"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 56
+        versionName = "v7.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
