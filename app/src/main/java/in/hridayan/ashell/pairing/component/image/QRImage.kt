05f910c6b2ae9b5529678e08db7e3c784b775830
DP-Hridayan <hridayanofficial@gmail.com>
2025-10-06 11:30:15 +0530

    UI improvements

--- app/src/main/java/in/hridayan/ashell/pairing/component/image/QRImage.kt
+++ app/src/main/java/in/hridayan/ashell/pairing/component/image/QRImage.kt
@@ -1,16 +1,20 @@
+@file:OptIn(ExperimentalMaterial3ExpressiveApi::class)
+
 package `in`.hridayan.ashell.pairing.component.image
 
 import android.graphics.Bitmap
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialShapes
+import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.unit.dp
@@ -24,8 +28,8 @@ fun QRImage(
 
     Box(
         modifier = modifier
-            .background(Color.White)
-            .border(width = 2.dp, color = MaterialTheme.colorScheme.primary),
+            .clip(MaterialShapes.Cookie9Sided.toShape())
+            .background(Color.White),
         contentAlignment = Alignment.Center
     ) {
         Image(
@@ -33,7 +37,7 @@ fun QRImage(
             contentDescription = "QR Code",
             modifier = Modifier
                 .size(200.dp)
-                .padding(25.dp)
+                .padding(50.dp)
         )
     }
 }
