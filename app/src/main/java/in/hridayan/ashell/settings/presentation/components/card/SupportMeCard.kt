6d2a7949edef59bd3037687b49021037eb45dc36
DP-Hridayan <hridayanofficial@gmail.com>
2025-10-28 22:55:54 +0530

    tweak some UI

--- app/src/main/java/in/hridayan/ashell/settings/presentation/components/card/SupportMeCard.kt
+++ app/src/main/java/in/hridayan/ashell/settings/presentation/components/card/SupportMeCard.kt
@@ -1,20 +1,17 @@
 package `in`.hridayan.ashell.settings.presentation.components.card
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
@@ -32,6 +29,8 @@ import androidx.compose.ui.unit.dp
 import `in`.hridayan.ashell.R
 import `in`.hridayan.ashell.core.common.LocalWeakHaptic
 import `in`.hridayan.ashell.core.common.constants.UrlConst
+import `in`.hridayan.ashell.core.presentation.components.card.RoundedCornerCard
+import `in`.hridayan.ashell.core.presentation.components.shape.CardCornerShape
 import `in`.hridayan.ashell.core.presentation.components.svg.DynamicColorImageVectors
 import `in`.hridayan.ashell.core.presentation.components.svg.vectors.bmcLogo
 import `in`.hridayan.ashell.core.utils.openUrl
@@ -39,60 +38,57 @@ import `in`.hridayan.ashell.core.utils.openUrl
 @Composable
 fun SupportMeCard(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .clip(MaterialTheme.shapes.large)
-            .clickable(enabled = true, onClick = {}),
-        colors = CardDefaults.cardColors(
-            containerColor = MaterialTheme.colorScheme.surfaceContainer,
-            contentColor = MaterialTheme.colorScheme.onSurface
-        )
+
+    Column(
+        modifier = modifier.fillMaxWidth()
     ) {
-        Column(
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            ContactHandles()
-
-            HorizontalDivider(thickness = 1.dp)
-
-            BuyMeACoffee(onClick = { openUrl(UrlConst.URL_DEV_BM_COFFEE, context) })
-        }
+        ContactHandles()
+        BuyMeACoffee(
+            onClick = { openUrl(UrlConst.URL_DEV_BM_COFFEE, context) },
+            modifier = Modifier.padding(top = 2.dp)
+        )
     }
 }
 
 @Composable
 private fun ContactHandles(modifier: Modifier = Modifier) {
     val context = LocalContext.current
-    Row(
-        modifier = modifier
-            .padding(horizontal = 25.dp, vertical = 10.dp)
-            .height(IntrinsicSize.Min)
+
+    RoundedCornerCard(
+        modifier = modifier,
+        roundedCornerShape = CardCornerShape.FIRST_CARD,
+        paddingValues = PaddingValues(0.dp)
     ) {
-        ContactBox(
-            modifier = Modifier.weight(1f),
-            painter = painterResource(R.drawable.ic_mail),
-            text = "E-mail",
-            onClick = { openUrl(url = UrlConst.URL_DEV_EMAIL, context = context) }
-        )
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 25.dp, vertical = 10.dp)
+                .height(IntrinsicSize.Min)
+        ) {
+            ContactBox(
+                modifier = Modifier.weight(1f),
+                painter = painterResource(R.drawable.ic_mail),
+                text = "E-mail",
+                onClick = { openUrl(url = UrlConst.URL_DEV_EMAIL, context = context) }
+            )
 
-        VerticalDivider(thickness = 1.dp, modifier = Modifier.padding(horizontal = 5.dp))
+            VerticalDivider(thickness = 1.dp, modifier = Modifier.padding(horizontal = 5.dp))
 
-        ContactBox(
-            modifier = Modifier.weight(1f),
-            painter = painterResource(R.drawable.ic_github),
-            text = stringResource(R.string.github),
-            onClick = { openUrl(url = UrlConst.URL_DEV_GITHUB, context = context) }
-        )
+            ContactBox(
+                modifier = Modifier.weight(1f),
+                painter = painterResource(R.drawable.ic_github),
+                text = stringResource(R.string.github),
+                onClick = { openUrl(url = UrlConst.URL_DEV_GITHUB, context = context) }
+            )
 
-        VerticalDivider(thickness = 1.dp, modifier = Modifier.padding(horizontal = 5.dp))
+            VerticalDivider(thickness = 1.dp, modifier = Modifier.padding(horizontal = 5.dp))
 
-        ContactBox(
-            modifier = Modifier.weight(1f),
-            painter = painterResource(R.drawable.ic_telegram),
-            text = "Telegram",
-            onClick = { openUrl(url = UrlConst.URL_DEV_TELEGRAM, context = context) }
-        )
+            ContactBox(
+                modifier = Modifier.weight(1f),
+                painter = painterResource(R.drawable.ic_telegram),
+                text = "Telegram",
+                onClick = { openUrl(url = UrlConst.URL_DEV_TELEGRAM, context = context) }
+            )
+        }
     }
 }
 
@@ -132,28 +128,35 @@ private fun ContactBox(
 @Composable
 private fun BuyMeACoffee(modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
     val weakHaptic = LocalWeakHaptic.current
-    Row(
-        horizontalArrangement = Arrangement.spacedBy(20.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier
-            .clickable(enabled = true, onClick = {
-                onClick()
-                weakHaptic()
-            })
-            .padding(horizontal = 25.dp, vertical = 15.dp)
-    ) {
-        Spacer(modifier = Modifier.weight(0.4f))
-        Image(
-            imageVector = DynamicColorImageVectors.bmcLogo(),
-            contentDescription = null,
-            modifier = Modifier.height(40.dp)
-        )
 
-        Text(
-            text = stringResource(R.string.support_bmc),
-            style = MaterialTheme.typography.bodySmall,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.weight(6f)
-        )
+    RoundedCornerCard(
+        modifier = modifier,
+        roundedCornerShape = CardCornerShape.LAST_CARD,
+        paddingValues = PaddingValues(0.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .clickable(enabled = true, onClick = {
+                    onClick()
+                    weakHaptic()
+                })
+                .padding(horizontal = 25.dp, vertical = 15.dp)
+        ) {
+            Spacer(modifier = Modifier.weight(0.4f))
+            Image(
+                imageVector = DynamicColorImageVectors.bmcLogo(),
+                contentDescription = null,
+                modifier = Modifier.height(40.dp)
+            )
+
+            Text(
+                text = stringResource(R.string.support_bmc),
+                style = MaterialTheme.typography.bodySmall,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.weight(6f)
+            )
+        }
     }
 }
