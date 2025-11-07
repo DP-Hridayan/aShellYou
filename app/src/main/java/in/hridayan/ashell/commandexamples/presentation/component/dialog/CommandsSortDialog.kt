7f6a7a829615c4b5c3bb8a53f9f95de4d3782d7d
DP-Hridayan <hridayanofficial@gmail.com>
2025-11-07 13:26:03 +0530

    ignore

--- app/src/main/java/in/hridayan/ashell/commandexamples/presentation/component/dialog/CommandsSortDialog.kt
+++ app/src/main/java/in/hridayan/ashell/commandexamples/presentation/component/dialog/CommandsSortDialog.kt
@@ -7,13 +7,11 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -24,7 +22,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -37,13 +34,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Dialog
-import androidx.compose.ui.window.DialogProperties
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import `in`.hridayan.ashell.R
 import `in`.hridayan.ashell.core.common.LocalWeakHaptic
 import `in`.hridayan.ashell.core.presentation.components.card.RoundedCornerCard
+import `in`.hridayan.ashell.core.presentation.components.dialog.DialogContainer
 import `in`.hridayan.ashell.core.presentation.components.shape.CardCornerShape.getRoundedShape
 import `in`.hridayan.ashell.core.presentation.components.text.AutoResizeableText
 import `in`.hridayan.ashell.settings.data.local.SettingsKeys
@@ -52,7 +49,6 @@ import `in`.hridayan.ashell.settings.presentation.viewmodel.SettingsViewModel
 
 @Composable
 fun CommandsSortDialog(
-    modifier: Modifier = Modifier,
     onDismiss: () -> Unit,
     settingsViewModel: SettingsViewModel = hiltViewModel(),
 ) {
@@ -68,136 +64,126 @@ fun CommandsSortDialog(
         tempSelected = selected.value
     }
 
-    Dialog(
-        onDismissRequest = { onDismiss() },
-        properties = DialogProperties(dismissOnClickOutside = true)
+    DialogContainer(
+        onDismiss = onDismiss
     ) {
-        Surface(
-            shape = MaterialTheme.shapes.extraLarge,
-            tonalElevation = 8.dp,
-            color = MaterialTheme.colorScheme.surfaceContainer
-        ) {
-            Column(
-                modifier = modifier
-                    .padding(24.dp)
-                    .widthIn(min = 280.dp)
-            ) {
-                AutoResizeableText(
-                    text = stringResource(R.string.sort),
-                    style = MaterialTheme.typography.titleLarge,
-                    color = MaterialTheme.colorScheme.onSurface,
-                    modifier = Modifier.padding(bottom = 24.dp)
-                )
+        AutoResizeableText(
+            text = stringResource(R.string.sort),
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 24.dp)
+        )
 
-                sortOptions.forEachIndexed { index, option ->
-                    val shape = getRoundedShape(index, sortOptions.size)
+        sortOptions.forEachIndexed { index, option ->
+            val shape = getRoundedShape(index, sortOptions.size)
 
-                    val selected = option.value == tempSelected
+            val selected = option.value == tempSelected
 
-                    val cardColors = if (selected) CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                    ) else CardDefaults.cardColors(
-                        containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
-                        contentColor = MaterialTheme.colorScheme.onSurface
-                    )
+            val cardColors = if (selected) CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            ) else CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surfaceContainerLowest,
+                contentColor = MaterialTheme.colorScheme.onSurface
+            )
 
-                    val animatedCorner by animateDpAsState(
-                        targetValue = if (selected) 32.dp else 4.dp,
-                        animationSpec = tween(
-                            durationMillis = 500,
-                            easing = FastOutSlowInEasing
-                        ),
-                        label = "cornerAnimation"
-                    )
+            val animatedCorner by animateDpAsState(
+                targetValue = if (selected) 32.dp else 4.dp,
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = FastOutSlowInEasing
+                ),
+                label = "cornerAnimation"
+            )
 
-                    val finalShape = if (selected) {
-                        RoundedCornerShape(animatedCorner)
-                    } else {
-                        shape
-                    }
+            val finalShape = if (selected) {
+                RoundedCornerShape(animatedCorner)
+            } else {
+                shape
+            }
 
-                    RoundedCornerCard(
-                        modifier = Modifier.fillMaxWidth(),
-                        roundedCornerShape = finalShape,
-                        paddingValues = PaddingValues(vertical = 1.dp),
-                        colors = cardColors
-                    )
-                    {
-                        Row(
-                            verticalAlignment = Alignment.CenterVertically,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .clickable {
-                                    tempSelected = option.value
-                                    weakHaptic()
-                                }
-                                .padding(vertical = 8.dp, horizontal = 20.dp)
-                        ) {
-                            Text(
-                                text = stringResource(option.labelResId),
-                                style = MaterialTheme.typography.bodyLarge,
-                                fontWeight = FontWeight.Bold
-                            )
-
-                            Spacer(Modifier.weight(1f))
-
-                            RadioButton(
-                                selected = (option.value == tempSelected),
-                                onClick = {
-                                    tempSelected = option.value
-                                    weakHaptic()
-                                },
-                                colors = RadioButtonDefaults.colors(
-                                    selectedColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                                    unselectedColor = MaterialTheme.colorScheme.onSurface
-                                )
-                            )
-                        }
-                    }
-                }
-
-                @Suppress("DEPRECATION")
-                ButtonGroup(
+            RoundedCornerCard(
+                modifier = Modifier.fillMaxWidth(),
+                roundedCornerShape = finalShape,
+                paddingValues = PaddingValues(vertical = 1.dp),
+                colors = cardColors
+            )
+            {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(top = 24.dp)
-                ) {
-                    OutlinedButton(
-                        modifier = Modifier
-                            .weight(1f)
-                            .animateWidth(interactionSources[0]),
-                        shapes = ButtonDefaults.shapes(),
-                        interactionSource = interactionSources[0],
-                        onClick = {
+                        .clickable {
+                            tempSelected = option.value
                             weakHaptic()
-                            onDismiss()
-                        },
-                        content = { AutoResizeableText(text = stringResource(R.string.cancel)) }
+                        }
+                        .padding(vertical = 8.dp, horizontal = 20.dp)
+                ) {
+                    Text(
+                        text = stringResource(option.labelResId),
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
                     )
 
-                    Button(
-                        modifier = Modifier
-                            .weight(1f)
-                            .animateWidth(interactionSources[1]),
-                        shapes = ButtonDefaults.shapes(),
-                        colors = ButtonDefaults.buttonColors(
-                            containerColor = MaterialTheme.colorScheme.primaryContainer,
-                            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
-                        ),
-                        interactionSource = interactionSources[1],
+                    Spacer(Modifier.weight(1f))
+
+                    RadioButton(
+                        selected = (option.value == tempSelected),
                         onClick = {
+                            tempSelected = option.value
                             weakHaptic()
-                            settingsViewModel.setInt(
-                                key = SettingsKeys.COMMAND_SORT_TYPE,
-                                value = tempSelected
-                            )
-                            onDismiss()
                         },
-                        content = { AutoResizeableText(text = stringResource(R.string.sort)) }
+                        colors = RadioButtonDefaults.colors(
+                            selectedColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                            unselectedColor = MaterialTheme.colorScheme.onSurface
+                        )
                     )
                 }
             }
+        }
+
+        @Suppress("DEPRECATION")
+        ButtonGroup(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 24.dp)
+        ) {
+            OutlinedButton(
+                modifier = Modifier
+                    .weight(1f)
+                    .animateWidth(interactionSources[0]),
+                shapes = ButtonDefaults.shapes(),
+                interactionSource = interactionSources[0],
+                onClick = {
+                    weakHaptic()
+                    onDismiss()
+                },
+                content = { AutoResizeableText(text = stringResource(R.string.cancel)) }
+            )
+
+            Button(
+                modifier = Modifier
+                    .weight(1f)
+                    .animateWidth(interactionSources[1]),
+                shapes = ButtonDefaults.shapes(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                ),
+                interactionSource = interactionSources[1],
+                onClick = {
+                    weakHaptic()
+                    settingsViewModel.setInt(
+                        key = SettingsKeys.COMMAND_SORT_TYPE,
+                        value = tempSelected
+                    )
+                    onDismiss()
+                },
+                content = { AutoResizeableText(text = stringResource(R.string.sort)) }
+            )
         }
     }
 }
