be65f0066775974e5941807b87c58cb1e6fabe03
DP-Hridayan <hridayanofficial@gmail.com>
2026-01-11 22:15:37 +0530

    ignore

--- app/src/main/java/in/hridayan/ashell/navigation/NavGraph.kt
+++ app/src/main/java/in/hridayan/ashell/navigation/NavGraph.kt
@@ -21,7 +21,6 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import `in`.hridayan.ashell.commandexamples.presentation.screens.CommandExamplesScreen
 import `in`.hridayan.ashell.core.common.LocalAnimatedContentScope
-import `in`.hridayan.ashell.core.common.LocalSharedTransitionScope
 import `in`.hridayan.ashell.core.domain.model.SharedTextHolder
 import `in`.hridayan.ashell.home.presentation.screens.HomeScreen
 import `in`.hridayan.ashell.onboarding.presentation.screens.OnboardingScreen
@@ -35,9 +34,9 @@ import `in`.hridayan.ashell.settings.presentation.page.crashhistory.screens.Cras
 import `in`.hridayan.ashell.settings.presentation.page.lookandfeel.screens.DarkThemeScreen
 import `in`.hridayan.ashell.settings.presentation.page.lookandfeel.screens.LookAndFeelScreen
 import `in`.hridayan.ashell.settings.presentation.page.mainscreen.screen.SettingsScreen
+import `in`.hridayan.ashell.shell.file_browser.presentation.screens.FileBrowserScreen
 import `in`.hridayan.ashell.shell.local_adb_shell.presentation.screens.LocalAdbScreen
 import `in`.hridayan.ashell.shell.otg_adb_shell.presentation.screens.OtgAdbScreen
-import `in`.hridayan.ashell.shell.file_browser.presentation.screens.FileBrowserScreen
 import `in`.hridayan.ashell.shell.wifi_adb_shell.presentation.screens.PairingOtherDeviceScreen
 import `in`.hridayan.ashell.shell.wifi_adb_shell.presentation.screens.PairingOwnDeviceScreen
 import `in`.hridayan.ashell.shell.wifi_adb_shell.presentation.screens.WifiAdbScreen
@@ -46,180 +45,176 @@ import kotlin.reflect.KType
 @Composable
 fun Navigation(isFirstLaunch: Boolean = false) {
     val navController = rememberNavController()
-    val sharedTransitionScope = LocalSharedTransitionScope.current
 
-    with(sharedTransitionScope) {
-        CompositionLocalProvider(
-            LocalNavController provides navController,
+    CompositionLocalProvider(
+        LocalNavController provides navController,
+    ) {
+        LaunchedEffect(Unit) {
+            SharedTextHolder.text?.let {
+                navController.navigate(NavRoutes.LocalAdbScreen)
+            }
+        }
+
+        NavHost(
+            navController = navController,
+            startDestination = if (isFirstLaunch) NavRoutes.OnboardingScreen else NavRoutes.HomeScreen
         ) {
-            LaunchedEffect(Unit) {
-                SharedTextHolder.text?.let {
-                    navController.navigate(NavRoutes.LocalAdbScreen)
-                }
+            composable<NavRoutes.OnboardingScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                OnboardingScreen()
             }
 
-            NavHost(
-                navController = navController,
-                startDestination = if (isFirstLaunch) NavRoutes.OnboardingScreen else NavRoutes.HomeScreen
+            composable<NavRoutes.HomeScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() }
             ) {
-                composable<NavRoutes.OnboardingScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    OnboardingScreen()
-                }
+                HomeScreen()
+            }
 
-                composable<NavRoutes.HomeScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() }
-                ) {
-                    HomeScreen()
-                }
+            composable<NavRoutes.SettingsScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                SettingsScreen()
+            }
 
-                composable<NavRoutes.SettingsScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    SettingsScreen()
-                }
+            composable<NavRoutes.LookAndFeelScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                LookAndFeelScreen()
+            }
 
-                composable<NavRoutes.LookAndFeelScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    LookAndFeelScreen()
-                }
+            composable<NavRoutes.DarkThemeScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                DarkThemeScreen()
+            }
 
-                composable<NavRoutes.DarkThemeScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    DarkThemeScreen()
-                }
+            composable<NavRoutes.BehaviorScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                BehaviorScreen()
+            }
 
-                composable<NavRoutes.BehaviorScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    BehaviorScreen()
-                }
+            composable<NavRoutes.AboutScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                AboutScreen()
+            }
 
-                composable<NavRoutes.AboutScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    AboutScreen()
-                }
-
-                composable<NavRoutes.CommandExamplesScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    CommandExamplesScreen()
-                }
+            composable<NavRoutes.CommandExamplesScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                CommandExamplesScreen()
+            }
 
 
-                composable<NavRoutes.ChangelogScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    ChangelogScreen()
-                }
+            composable<NavRoutes.ChangelogScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                ChangelogScreen()
+            }
 
-                animatedComposable<NavRoutes.CrashHistoryScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    CrashHistoryScreen()
-                }
+            animatedComposable<NavRoutes.CrashHistoryScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                CrashHistoryScreen()
+            }
 
-                animatedComposable<NavRoutes.CrashDetailsScreen>(
-                ) {
-                    CrashDetailsScreen()
-                }
+            animatedComposable<NavRoutes.CrashDetailsScreen> {
+                CrashDetailsScreen()
+            }
 
-                composable<NavRoutes.AutoUpdateScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    AutoUpdateScreen()
-                }
+            composable<NavRoutes.AutoUpdateScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                AutoUpdateScreen()
+            }
 
-                composable<NavRoutes.BackupAndRestoreScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    BackupAndRestoreScreen()
-                }
+            composable<NavRoutes.BackupAndRestoreScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                BackupAndRestoreScreen()
+            }
 
-                composable<NavRoutes.LocalAdbScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    LocalAdbScreen()
-                }
+            composable<NavRoutes.LocalAdbScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                LocalAdbScreen()
+            }
 
-                composable<NavRoutes.OtgAdbScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    OtgAdbScreen()
-                }
+            composable<NavRoutes.OtgAdbScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                OtgAdbScreen()
+            }
 
-                composable<NavRoutes.PairingOwnDeviceScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    PairingOwnDeviceScreen()
-                }
+            composable<NavRoutes.PairingOwnDeviceScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                PairingOwnDeviceScreen()
+            }
 
-                composable<NavRoutes.PairingOtherDeviceScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    PairingOtherDeviceScreen()
-                }
+            composable<NavRoutes.PairingOtherDeviceScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                PairingOtherDeviceScreen()
+            }
 
-                composable<NavRoutes.WifiAdbScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) {
-                    WifiAdbScreen()
-                }
+            composable<NavRoutes.WifiAdbScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) {
+                WifiAdbScreen()
+            }
 
-                composable<NavRoutes.FileBrowserScreen>(
-                    enterTransition = { slideFadeInFromRight() },
-                    exitTransition = { slideFadeOutToLeft() },
-                    popEnterTransition = { slideFadeInFromLeft() },
-                    popExitTransition = { slideFadeOutToRight() }
-                ) { backStackEntry ->
-                    val route = backStackEntry.toRoute<NavRoutes.FileBrowserScreen>()
-                    FileBrowserScreen(
-                        deviceAddress = route.deviceAddress,
-                        isOwnDevice = route.isOwnDevice
-                    )
-                }
+            composable<NavRoutes.FileBrowserScreen>(
+                enterTransition = { slideFadeInFromRight() },
+                exitTransition = { slideFadeOutToLeft() },
+                popEnterTransition = { slideFadeInFromLeft() },
+                popExitTransition = { slideFadeOutToRight() }
+            ) { backStackEntry ->
+                val route = backStackEntry.toRoute<NavRoutes.FileBrowserScreen>()
+                FileBrowserScreen(
+                    deviceAddress = route.deviceAddress,
+                    isOwnDevice = route.isOwnDevice
+                )
             }
         }
     }
