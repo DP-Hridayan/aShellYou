a8fb26d874bf52e786a688b61071f1582f474c9b
DP-Hridayan <hridayanofficial@gmail.com>
2026-01-14 13:30:38 +0530

    added gradient to changelog bottomsheet

--- app/src/main/java/in/hridayan/ashell/core/presentation/components/bottomsheet/ChangelogBottomSheet.kt
+++ app/src/main/java/in/hridayan/ashell/core/presentation/components/bottomsheet/ChangelogBottomSheet.kt
@@ -3,6 +3,8 @@
 package `in`.hridayan.ashell.core.presentation.components.bottomsheet
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -17,8 +19,11 @@ import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -56,53 +61,70 @@ fun ChangelogBottomSheet(
             fontWeight = FontWeight.Bold
         )
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxWidth()
-                .heightIn(max = screenHeight * 0.5f),
-        ) {
-            item {
-                Spacer(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(25.dp)
-                )
-            }
-            
-            itemsIndexed(items = recentChangelog) { index, item ->
-                val isLatestVersion =
-                    item.versionName == BuildConfig.VERSION_NAME.removeSuffix("-debug")
-
-                AutoResizeableText(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 25.dp),
-                    text = stringResource(R.string.version) + "\t\t${item.versionName}",
-                    style =
-                        if (isLatestVersion) MaterialTheme.typography.headlineMedium else MaterialTheme.typography.headlineSmall,
-                    color =
-                        if (isLatestVersion) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface,
-                    fontWeight = FontWeight.Bold
-                )
-
-                BulletPointsTextLayout(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(25.dp),
-                    textLines = splitStringToLines(item.changelog)
-                )
-
-                if (index != recentChangelog.lastIndex) {
-                    HorizontalDivider(
+        Box(modifier = Modifier.fillMaxWidth()) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .heightIn(max = screenHeight * 0.5f),
+            ) {
+                item {
+                    Spacer(
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(25.dp)
-                            .alpha(0.5f),
-                        color = MaterialTheme.colorScheme.onSurface,
-                        thickness = 1.dp
+                            .height(25.dp)
                     )
                 }
+
+                itemsIndexed(items = recentChangelog) { index, item ->
+                    val isLatestVersion =
+                        item.versionName == BuildConfig.VERSION_NAME.removeSuffix("-debug")
+
+                    AutoResizeableText(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 25.dp),
+                        text = stringResource(R.string.version) + "\t\t${item.versionName}",
+                        style =
+                            if (isLatestVersion) MaterialTheme.typography.headlineMedium else MaterialTheme.typography.headlineSmall,
+                        color =
+                            if (isLatestVersion) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    BulletPointsTextLayout(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(25.dp),
+                        textLines = splitStringToLines(item.changelog)
+                    )
+
+                    if (index != recentChangelog.lastIndex) {
+                        HorizontalDivider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(25.dp)
+                                .alpha(0.5f),
+                            color = MaterialTheme.colorScheme.onSurface,
+                            thickness = 1.dp
+                        )
+                    }
+                }
             }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp)
+                    .align(Alignment.BottomCenter)
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                MaterialTheme.colorScheme.background
+                            )
+                        )
+                    )
+            )
         }
     }
 }
