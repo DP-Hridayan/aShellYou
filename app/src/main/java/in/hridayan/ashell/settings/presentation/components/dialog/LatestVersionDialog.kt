e2555a57ccb4ec58d3e2346c2e58e3b335c43394
DP-Hridayan <hridayanofficial@gmail.com>
2026-04-25 16:14:25 +0530

    improved UI

--- app/src/main/java/in/hridayan/ashell/settings/presentation/components/dialog/LatestVersionDialog.kt
+++ app/src/main/java/in/hridayan/ashell/settings/presentation/components/dialog/LatestVersionDialog.kt
@@ -2,13 +2,8 @@
 
 package `in`.hridayan.ashell.settings.presentation.components.dialog
 
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateIntAsState
-import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -17,8 +12,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
@@ -27,8 +20,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.toShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -40,10 +31,11 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import `in`.hridayan.ashell.BuildConfig
 import `in`.hridayan.ashell.R
+import `in`.hridayan.ashell.core.presentation.components.card.CustomCard
 import `in`.hridayan.ashell.core.presentation.components.dialog.DialogContainer
 import `in`.hridayan.ashell.core.presentation.components.haptic.withHaptic
 import `in`.hridayan.ashell.core.presentation.components.text.AutoResizeableText
-import `in`.hridayan.ashell.core.presentation.theme.AshellYouAnimationSpecs
+import `in`.hridayan.ashell.core.presentation.theme.CustomCardShape
 import `in`.hridayan.ashell.core.presentation.utils.syncedRotationAndScale
 
 @Composable
@@ -99,23 +91,8 @@ fun LatestVersionDialog(onDismiss: () -> Unit) {
 
 @Composable
 private fun AnimatedInfoCard(text: String) {
-    val interactionSource = remember { MutableInteractionSource() }
-    val isPressed by interactionSource.collectIsPressedAsState()
-
-    val roundedCornerPercentage by animateIntAsState(
-        targetValue = if (isPressed) 15 else 50,
-        animationSpec = AshellYouAnimationSpecs.springInt,
-        label = "corner_anim"
-    )
-
-    val animatedRoundedCornerShape =
-        RoundedCornerShape(roundedCornerPercentage.coerceIn(0, 100))
-
-    Card(
-        modifier = Modifier
-            .clip(animatedRoundedCornerShape)
-            .clickable(interactionSource = interactionSource) {},
-        shape = animatedRoundedCornerShape,
+    CustomCard(
+        shape = CustomCardShape(50),
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.tertiaryContainer,
             contentColor = MaterialTheme.colorScheme.onTertiaryContainer
