dfa3f4cf15733829b912ee5a93a070a499ffd082
DP-Hridayan <hridayanofficial@gmail.com>
2026-08-28 21:11:49 +0530

    Update version name to v8.0.0-alpha08

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ android {
         minSdk = 28
         targetSdk = 37
         versionCode = 63
-        versionName = "v8.0.0-alpha07"
+        versionName = "v8.0.0-alpha08"
 
         buildConfigField("String", "DIST_FLAVOR_GITHUB", "\"$flavorGitHub\"")
         buildConfigField("String", "DIST_FLAVOR_FDROID", "\"$flavorFDroid\"")
