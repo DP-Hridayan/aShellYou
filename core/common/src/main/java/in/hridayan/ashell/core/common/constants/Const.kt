44170ea2ac488b4b34e1cccb6b502c11aff25d84
DP-Hridayan <hridayanofficial@gmail.com>
2026-09-04 14:27:15 +0530

    Added privacy policy update dialog

--- core/common/src/main/java/in/hridayan/ashell/core/common/constants/Const.kt
+++ core/common/src/main/java/in/hridayan/ashell/core/common/constants/Const.kt
@@ -4,3 +4,4 @@ const val SHIZUKU_PACKAGE_NAME = "moe.shizuku.privileged.api"
 const val SHIZUKU_PLUS_PACKAGE_NAME = "af.shizuku.plus.api"
 const val DEV_EMAIL = "hridayanofficial@gmail.com"
 const val DEV_GITHUB_USERNAME = "DP-Hridayan"
+const val PRIVACY_POLICY_VERSION = 1
