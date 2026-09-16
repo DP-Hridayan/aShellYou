1ef6e1ca612a7124033b437e5b62276973c3c5e6
Hridayan <157479796+DP-Hridayan@users.noreply.github.com>
2026-09-16 19:50:31 +0530

    Delete test.kt

--- test.kt
+++
@@ -1,7 +0,0 @@
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.LinkAnnotation
-
-fun test() {
-    val b = AnnotatedString.Builder()
-    b.addLink(LinkAnnotation.Clickable("tag") { }, 0, 1)
-}
